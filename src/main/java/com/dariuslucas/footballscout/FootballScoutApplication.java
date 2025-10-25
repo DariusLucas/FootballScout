863a31bcf7853b5138e117205fb545922e4344fb
Darius Lucas <dariusbalogh0@gmail.com>
2025-10-25 12:58:55 +0300

    Initial commit

---
+++ src/main/java/com/dariuslucas/footballscout/FootballScoutApplication.java
@@ -0,0 +1,13 @@
+package com.dariuslucas.footballscout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballScoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballScoutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dariuslucas/footballscout/FootballScoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dariuslucas.footballscout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballScoutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
